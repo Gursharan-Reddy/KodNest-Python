d87091838da354c68a65f6de0b3d27e094a6b201
Gursharan-Reddy <balareddygursharan8@gmail.com>
2026-08-03 17:15:05 +0530

    varaibles and datatypes

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/modules/module_2/Student_datatypes.py
@@ -0,0 +1,41 @@
+# Question
+# Inspect Student Data Types
+# A course registration system stores different kinds of student information.
+
+# Write a Python program that creates variables for a student’s name, course duration, course rating and enrolment status.
+
+# Use type() to display the data type of each variable.
+
+# Create the following variables:
+
+# student_name with the value "Aarav"
+
+# course_duration with the value 30
+
+# course_rating with the value 4.8
+
+# is_enrolled with the value True
+
+# Input
+# The program does not require any input.
+
+# Output
+# Print the data type of each variable in the following order:
+
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'bool'>
+
+# Create the required variables
+
+# Display the data type of each variable
+student_name = "Aarav"
+course_duration = 30
+course_rating = 4.8
+is_enrolled = True
+
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/modules/module_2/pseudocode_to_python.py
@@ -0,0 +1,20 @@
+# Question
+# Convert Pseudocode to Python
+# You are given three pseudocode instructions that display messages.
+
+# Complete the Python program step by step by converting each pseudocode instruction into a valid Python print() statement. In every sub-task, add only the requested single line of code.
+
+# Pseudocode
+# PRINT "Welcome to KodNest"
+# PRINT "Python Track Started"
+# PRINT "Let us begin coding"
+# After completing all the sub-tasks, the program must display:
+
+# Welcome to KodNest
+# Python Track Started
+# Let us begin coding
+
+# Add the required lines here
+print("Welcome to KodNest")
+print("Python Track Started")
+print("Let us begin coding")
